8566ef00e781428961ffc719e657ac0bf35d282e
illia goncharov <i.goncharov@technoji.io>
2024-05-13 18:29:29 -0700

    fix pagination

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -68,6 +68,7 @@ const Games = () => {
                     {item}
                   </th>
                 ))}
+                <th></th>
               </tr>
             </thead>
             <tbody>
@@ -81,6 +82,14 @@ const Games = () => {
                       {game[key as keyof Game]}
                     </td>
                   ))}
+                  <td className="px-6 py-4">
+                    <button
+                      type="button"
+                      className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-1 text-center"
+                    >
+                      Delete
+                    </button>
+                  </td>
                 </tr>
               ))}
             </tbody>
